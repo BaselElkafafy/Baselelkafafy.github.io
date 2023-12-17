3f18107951b5961900f8b781de36b9686e2f2a06
BaselElkafafy <154018816+BaselElkafafy@users.noreply.github.com>
2023-12-17 13:41:56 +0200

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import StarsCanvas from "@/components/main/StarsBackground";
+import Navbar from "@/components/Navbar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Basel Elkafafy",
+  description: "Developer Portfolio By Basel Elkafafy",
+  keywords: ["Developer", "Portfolio", "Developer Portflio", "Basel Elkafafy"],
+  openGraph: {
+    title: "Basel Elkafafy",
+    description: "Web Designer",
+    images: "/OpenGraph.jpg",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.className} bg-[#111] overflow-y-scroll overflow-x-hidden`}
+      >
+        <StarsCanvas />
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}
