c22e948187afc6f2d7c128aea8dfd63ae7a704ca
BaselElkafafy <154018816+BaselElkafafy@users.noreply.github.com>
2023-12-18 04:13:44 -0800

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-      <main className="h-full w-full bg-[url('/LooperGroup2.png')] bg-no-repeat">
+       <main className="h-full w-full bg-[url('/LooperGroup2.png')] bg-no-repeat">
       <div className="flex flex-col gap-20">
         <Banner />
         <About />
