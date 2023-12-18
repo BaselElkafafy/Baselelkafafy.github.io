a57b3f30d45158f517331b9a4ad080333db5d5c8
BaselElkafafy <154018816+BaselElkafafy@users.noreply.github.com>
2023-12-18 05:53:20 -0800

    Update About.tsx

--- components/About.tsx
+++ components/About.tsx
@@ -5,7 +5,7 @@ const About = () => {
     <section
       id="about"
       className="flex flex-col items-center justify-center h-full relative overflow-hidden pt-[35px] "
-      style={{ transform: "scale(0.9" }}
+      style={{ transform: "scale(0.9)" }}
     >
       <div className="flex flex-col  justify-around flex-wrap items-center   max-w-[900px]">
         <h1 className="text-white font-semibold text-6xl">ABOUT ME</h1>
@@ -13,13 +13,12 @@ const About = () => {
           EXPLORE NOW
         </p>
         <p className="text-gray-300 text-center ">
-          As a dedicated E-commerce Web Designer, Basel Elkafafy, I'm fueled by the dynamic intersection of design
-         innovation and user-centric functionality.
-         Specializing in crafting visually captivating online shopping experiences,
+          As a dedicated E-commerce Web Designer, Basel Elkafafy, I&rsquo;m fueled by the dynamic intersection of design
+          innovation and user-centric functionality. Specializing in crafting visually captivating online shopping experiences,
           <br />
           <br />
           I navigate the intricate dance between aesthetics and seamless functionality.
-          My commitment is to enhance the user journey, 
+          My commitment is to enhance the user journey,
           contributing to the success of online businesses through a harmonious blend of creativity and technical expertise.
         </p>
       </div>
