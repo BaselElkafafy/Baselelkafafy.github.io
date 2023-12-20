83bdeb5db9309ead95a7d1693eb95add5fd41776
Basel Elkafafy <154018816+BaselElkafafy@users.noreply.github.com>
2023-12-20 03:12:39 -0800

    Update Projects.tsx

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import Link from "next/link";
 import React from "react";
 
 const Projects = () => {
@@ -8,23 +7,41 @@ const Projects = () => {
       <h1 className="text-white font-semibold text-center text-6xl pt-[35px]">
         PROJECTS
       </h1>
-      <p className="tracking-[0.5em] text-center text-transparent font-light pb-5 bg-clip-text bg-gradient-to-r from-purple-700 to-orange-500 text-1xl">
+      <p className="tracking-[0.5em] text-center text-transparent font-light pb-5 bg-clip-text bg-gradient-to-r from-purple-700 to-orange-500 text-xl">
         EXPLORE NOW FIGMA DESIGNS
       </p>
       <div className="container mx-auto 2xl">
-        <div className="flex-col flex md:flex-row  mt-7">
+        <div className="flex-col mt-7">
           <div className="flex-row flex mb-5">
-            <Image
-              src="/FirstProject.png"
-              height={1200}
-              width={1600}
-              alt="BMW I8 Landing Page"
-            />
-            <Image
-              src="/SecondProject.png"
-              height={1200}
-              width={1600}
-            />
+            <div className="p-3">
+              <Image
+                src="/FirstProject.png"
+                height={1200}
+                width={1600}
+                alt="BMW I8 Landing Page"
+              />
+            </div>
+            <div className="p-3">
+              <p className="text-white font-semibold text-xl">BMW I8</p>
+              <p className="text-gray-500 text-[10px]">
+                Thrilled to share the BMW i8 UI landing page designed with precision in Figma.
+              </p>
+            </div>
+          </div>
+          <div className="flex-row flex mb-5">
+            <div className="p-3">
+              <Image
+                src="/SecondProject.png"
+                height={1200}
+                width={1600}
+                alt="HireSide"
+              />
+            </div>
+            <div className="p-3">
+              <p className="text-white font-semibold text-xl">iPhone 15 Pro Landing page</p>
+              <p className="text-gray-500 text-[10px]">
+                Thrilled to share the iPhone 15 Pro UI landing page Example designed with precision in Figma.
+              </p>
             </div>
           </div>
         </div>
