6ccc8519734726b3cf078519278558a05f33aa72
Basel Elkafafy <154018816+BaselElkafafy@users.noreply.github.com>
2023-12-20 02:58:23 -0800

    Update Projects.tsx

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -10,7 +10,7 @@ const Projects = () => {
         PROJECTS
       </h1>
       <p className="tracking-[0.5em] text-center text-transparent font-light pb-5 bg-clip-text bg-gradient-to-r from-purple-700 to-orange-500 text-1xl">
-        EXPLORE NOW
+        EXPLORE NOW FIGMA DESIGNS
       </p>
       <div className="container mx-auto 2xl">
         <div className="flex-col flex md:flex-row  mt-7">
