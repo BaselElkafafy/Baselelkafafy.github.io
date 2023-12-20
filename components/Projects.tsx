a13604c519509ee6dabc19a3afd309c7efa8e157
Basel Elkafafy <154018816+BaselElkafafy@users.noreply.github.com>
2023-12-20 03:00:32 -0800

    Update Projects.tsx

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -6,7 +6,6 @@ const Projects = () => {
   return (
     <section id="projects">
       <h1 className="text-white font-semibold text-center text-6xl pt-[35px]">
-        
         PROJECTS
       </h1>
       <p className="tracking-[0.5em] text-center text-transparent font-light pb-5 bg-clip-text bg-gradient-to-r from-purple-700 to-orange-500 text-1xl">
@@ -48,4 +47,5 @@ const Projects = () => {
   );
 };
 
+export default Projects;
 export default Projects;
