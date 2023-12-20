ef259356750e71bb3f0e0674cb7a19ff736ed2b7
Basel Elkafafy <154018816+BaselElkafafy@users.noreply.github.com>
2023-12-20 03:08:49 -0800

    Update Projects.tsx

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -20,25 +20,11 @@ const Projects = () => {
               width={1600}
               alt="BMW I8 Landing Page"
             />
-            <div className="p-3">
-              <p className="text-white font-semibold text-xl">BMW I8</p>
-              <p className="text-gray-500 text-[10px]">
-                Thrilled to share the BMW i8 UI landing page designed with precision in Figma.
-              </p>
-            </div>
-          </div>
-          <div className="flex-row flex mb-5">
             <Image
               src="/SecondProject.png"
               height={1200}
               width={1600}
-              alt="HireSide"
             />
-            <div className="p-3">
-              <p className="text-white font-semibold text-xl">iPhone 15 Pro Landing page</p>
-              <p className="text-gray-500 text-[10px]">
-                Thrilled to share the iPhone 15 Pro UI landing page Example designed with precision in Figma.
-              </p>
             </div>
           </div>
         </div>
