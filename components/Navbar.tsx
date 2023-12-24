aecbdb8dd5401d0ec0372cef06690b8fad9fdd72
Basel Elkafafy <154018816+BaselElkafafy@users.noreply.github.com>
2023-12-24 11:18:12 -0800

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,19 +1,16 @@
+"use client";
+
 import Image from "next/image";
-import Link from "next/link";
-import { useNavigation } from 'next/navigation';
 import React from "react";
 
 const Navbar = () => {
-  const navigation = useNavigation();
-
-  const navigateToPricing = () => {
-    navigation.navigate('/pricing');
-  };
-
   return (
     <div className="w-full h-[65px] bg-['#111'] fixed backdrop-blur-sm z-50 px-10">
       <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
-        <a href="#about-me" className="h-auto w-auto flex flex-row items-center">
+        <a
+          href="#about-me"
+          className="h-auto w-auto flex flex-row items-center"
+        >
           <Image
             src="/Logo.svg"
             alt="logo"
@@ -26,16 +23,9 @@ const Navbar = () => {
         <div className="flex flex-row gap-5">
           <div
             onClick={() => window.open("mailto:official.baselhassan@gmail.com")}
-            className="z-[1] bg-transparent padding-10 cursor-pointer bg-black hover:bg-[#2E2E2E] rounded-xl text-white py-2 px-5"
+            className=" z-[1] bg-transparent  padding-10 cursor-pointer bg-black hover:bg-[#2E2E2E] rounded-xl  text-white  py-2 px-5"
           >
             Contact
-          </div>
-
-          <div
-            onClick={navigateToPricing}
-            className="z-[1] bg-transparent padding-10 cursor-pointer bg-black hover:bg-[#2E2E2E] rounded-xl text-white py-2 px-5"
-          >
-            Go to Pricing
           </div>
         </div>
       </div>
