f371b1bb5da5e743b32fba85b7e8569ea4e5f9d8
Basel Elkafafy <154018816+BaselElkafafy@users.noreply.github.com>
2023-12-24 10:25:29 -0800

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,11 @@
 import Image from "next/image";
 import React from "react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const Navbar = () => {
+  const router = useRouter();
+
   return (
     <div className="w-full h-[65px] bg-['#111'] fixed backdrop-blur-sm z-50 px-10">
       <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
@@ -19,16 +22,18 @@ const Navbar = () => {
         </Link>
 
         <div className="flex flex-row gap-5">
-          <div className="z-[1] bg-transparent padding-10 cursor-pointer bg-black hover:bg-[#2E2E2E] rounded-xl text-white py-2 px-5">
-            <a href="mailto:official.baselhassan@gmail.com">Contact</a>
+          <div
+            onClick={() => window.open("mailto:official.baselhassan@gmail.com")}
+            className="z-[1] bg-transparent padding-10 cursor-pointer bg-black hover:bg-[#2E2E2E] rounded-xl text-white py-2 px-5"
+          >
+            Contact
           </div>
-
-          {/* New button to navigate to Pricing page with the same style */}
-          <Link href="/pricing">
-            <a className="z-[1] bg-transparent padding-10 cursor-pointer bg-black hover:bg-[#2E2E2E] rounded-xl text-white py-2 px-5">
-              Pricing
-            </a>
-          </Link>
+          <div
+            onClick={() => router.push("/pricing")} // Updated navigation
+            className="z-[1] bg-transparent padding-10 cursor-pointer bg-black hover:bg-[#2E2E2E] rounded-xl text-white py-2 px-5"
+          >
+            Pricing
+          </div>
         </div>
       </div>
     </div>
