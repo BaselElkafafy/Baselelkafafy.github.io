a6d5d51bd7af0079e0e84b9e9f5a57adfdd04c1c
Basel Elkafafy <154018816+BaselElkafafy@users.noreply.github.com>
2023-12-24 10:29:02 -0800

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,8 @@
 import Image from "next/image";
 import React from "react";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
 
 const Navbar = () => {
-  const router = useRouter();
-
   return (
     <div className="w-full h-[65px] bg-['#111'] fixed backdrop-blur-sm z-50 px-10">
       <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
@@ -28,12 +25,12 @@ const Navbar = () => {
           >
             Contact
           </div>
-          <div
-            onClick={() => router.push("/pricing")} // Updated navigation
-            className="z-[1] bg-transparent padding-10 cursor-pointer bg-black hover:bg-[#2E2E2E] rounded-xl text-white py-2 px-5"
-          >
-            Pricing
-          </div>
+          
+          <Link href="/pricing">
+            <a className="z-[1] bg-transparent padding-10 cursor-pointer bg-black hover:bg-[#2E2E2E] rounded-xl text-white py-2 px-5">
+              Pricing
+            </a>
+          </Link>
         </div>
       </div>
     </div>
