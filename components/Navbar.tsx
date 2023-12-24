913d70bdee79a951250924a3c17cb3e46b192c8d
Basel Elkafafy <154018816+BaselElkafafy@users.noreply.github.com>
2023-12-24 10:43:05 -0800

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,40 +1,181 @@
-import Image from "next/image";
 import React from "react";
+import Navbar from "../components/Navbar";
 import Link from "next/link";
 
-const Navbar = () => {
+function PricingPage({ className = "" }: PricingPageProps) {
   return (
-    <div className="w-full h-[65px] bg-['#111'] fixed backdrop-blur-sm z-50 px-10">
-      <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
-        <Link href="#about-me">
-          <a className="h-auto w-auto flex flex-row items-center">
-            <Image
-              src="/Logo.svg"
-              alt="logo"
-              width={100}
-              height={100}
-              className="cursor-pointer hover:animate-slowspin"
-            />
-          </a>
-        </Link>
+    <div className={`relative ${className}`}>
+      <Navbar />
 
-        <div className="flex flex-row gap-5">
-          <div
-            onClick={() => window.open("mailto:official.baselhassan@gmail.com")}
-            className="z-[1] bg-transparent padding-10 cursor-pointer bg-black hover:bg-[#2E2E2E] rounded-xl text-white py-2 px-5"
-          >
-            Contact
+      <div className="inline-flex h-[3518px] w-[1440px] resize flex-row overflow-hidden">
+        export default function PricingPage({ className = "" }: PricingPageProps) {
+  return (
+    <div className="inline-flex h-[3518px] w-[1440px] resize flex-row overflow-hidden" >
+      <div className="relative flex h-full w-full items-start overflow-clip bg-black" >
+        <div className="bg-background-pattern absolute right-[-915.91px] top-[-290px] flex h-[2609px] w-[2609px] items-end bg-cover bg-center pr-[1420px] pb-64 pl-96 pt-96" >
+          <div className="font-poppins absolute left-96 top-80 flex h-20 w-[1104px] items-center justify-center" >
+            <div className="mr-0 flex flex-grow items-start justify-center gap-x-6 self-stretch" >
+              <div className="relative flex w-44 items-center self-stretch pr-24 text-left text-white" >
+                <div className="absolute right-12 top-5 h-8 w-16 text-[22px] font-bold leading-[normal] tracking-[0px]" >
+                  Basel
+                </div>
+                <div className="absolute bottom-5 right-0 h-3.5 w-20 text-[9.2px] font-normal leading-[normal] tracking-[9.6px]" >
+                  <span className="uppercase">Elkafafy</span>
+                </div>
+                <div className="bg-untitled-design-41 relative flex flex-grow items-start self-stretch bg-cover bg-center" />
+              </div>
+              <div className="flex flex-grow items-center justify-end pl-96 text-center text-[13px] font-medium leading-[normal] tracking-[0px] text-black" >
+                <div className="flex flex-grow items-center justify-center self-stretch" >
+                  <div className="flex flex-grow justify-center self-stretch rounded-lg bg-white py-2 px-6" >
+                    Home
+                  </div>
+                </div>
+              </div>
+              <div className="flex items-center justify-center text-center text-[13px] font-medium leading-[normal] tracking-[0px] text-black" >
+                <div className="flex flex-grow justify-center self-stretch rounded-lg bg-white py-2 px-4" >
+                  Contact
+                </div>
+              </div>
+            </div>
           </div>
-          
-          <Link href="/pricing">
-            <a className="z-[1] bg-transparent padding-10 cursor-pointer bg-black hover:bg-[#2E2E2E] rounded-xl text-white py-2 px-5">
-              Pricing
-            </a>
-          </Link>
+          <div className="relative mr-[-643.93px] flex flex-grow flex-col items-start justify-center self-stretch" >
+            <div className="font-poppins flex justify-center text-center text-[63px] font-bold leading-[normal] tracking-[-0.96px] text-white" >
+              PRICING
+            </div>
+            <div className="font-inter flex h-4 flex-col items-center justify-end text-center text-[26px] font-semibold leading-[normal] tracking-[0px] text-neutral-500" >
+              <div className="flex justify-center self-stretch">
+                E-commerce Prices
+              </div>
+            </div>
+            <div className="font-poppins flex flex-col items-center justify-end pt-28 text-center text-5xl font-bold leading-[normal] tracking-[-0.96px]" >
+              <div className="flex flex-grow justify-center self-stretch">
+                Basic Package
+              </div>
+            </div>
+            <div className="font-poppins flex flex-col items-center justify-end pt-10 text-center font-bold tracking-[-0.96px] text-white" >
+              <div className="flex w-96 flex-grow rounded-3xl">
+                <div className="relative flex h-full w-full items-start justify-end overflow-clip rounded-3xl bg-gradient-to-b from-black to-white/0 pb-80" >
+                  <div className="relative ml-[-50px] flex flex-col items-start justify-center gap-y-14 self-stretch" >
+                    <div className="flex justify-center text-5xl leading-[normal]" >
+                      3,699 L . E
+                    </div>
+                    <div className="flex justify-center self-stretch text-[32px] leading-[normal]" >
+                      <ul className="ml-[1em] list-disc">
+                        <li className="mb-0">
+                          Customizable template design
+                        </li>
+                        <li className="mb-0">Mobile responsiveness</li>
+                        <li className="mb-0">Basic SEO optimization</li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div className="absolute right-[0.01px] left-0 top-16 h-0.5" >
+                    <svg width="100%" height="100%" style={{"overflow":"visible"}} preserveAspectRatio="none" viewBox="0 2 393.99 1.5" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0 4.5L393.992 3" stroke="url(#paint0_linear_249_308)" stroke-width="5" stroke-linecap="round"/><defs><linearGradient id="paint0_linear_249_308" x1="197.002" y1="5.25001" x2="197.006" y2="6.25" gradientUnits="userSpaceOnUse"><stop stop-color="#A1A1A1"/><stop offset="1" stop-opacity="0"/></linearGradient></defs></svg>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="font-poppins flex flex-grow flex-col items-center justify-end self-stretch pt-28 font-bold tracking-[-0.96px] text-white" >
+              <div className="flex flex-grow flex-col items-center justify-between gap-y-7 self-stretch" >
+                <div className="flex justify-center self-stretch text-center text-5xl leading-[normal]" >
+                  Standard Package
+                </div>
+                <div className="flex w-96 flex-grow rounded-3xl">
+                  <div className="flex h-full w-full items-start justify-center overflow-clip rounded-3xl bg-gradient-to-b from-black to-white/0 pb-44" >
+                    <div className="-mr-2.5 flex flex-grow flex-col items-center justify-center self-stretch" >
+                      <div className="flex justify-center text-center text-5xl leading-[normal]" >
+                        6,169 L . E
+                      </div>
+                      <div className="flex h-0 flex-col items-center justify-center self-stretch" >
+                        <div className="h-0.5 w-96">
+                          <svg width="100%" height="100%" style={{"overflow":"visible"}} preserveAspectRatio="none" viewBox="0 2 393.99 1.5" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0 4L393.992 2.5" stroke="url(#paint0_linear_249_305)" stroke-width="5" stroke-linecap="round"/><defs><linearGradient id="paint0_linear_249_305" x1="197.002" y1="4.75001" x2="197.006" y2="5.75" gradientUnits="userSpaceOnUse"><stop stop-color="#A1A1A1"/><stop offset="1" stop-opacity="0"/></linearGradient></defs></svg>
+                        </div>
+                      </div>
+                      <div className="flex flex-grow self-stretch pt-8 text-left text-[32px] leading-[normal]" >
+                        <ul className="ml-[1em] list-disc">
+                          <li className="mb-0">Enhanced custom design</li>
+                          <li className="mb-0">
+                            E-commerce functionality (limited products)
+                          </li>
+                          <li className="mb-0">Blog integration</li>
+                          <li className="mb-0">
+                            Social media integration
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="absolute bottom-14 left-96 h-4 w-16">
+          <svg width="100%" height="100%" style={{"overflow":"visible"}} preserveAspectRatio="none" viewBox="0 0 66.8 14.58" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.68049 10.257C8.46793 10.257 9.22313 9.94417 9.77994 9.38736C10.3367 8.83055 10.6496 8.07536 10.6496 7.28791C10.6496 6.50047 10.3367 5.74527 9.77994 5.18847C9.22313 4.63166 8.46793 4.31885 7.68049 4.31885C6.89305 4.31885 6.13785 4.63166 5.58104 5.18847C5.02424 5.74527 4.71143 6.50047 4.71143 7.28791C4.71143 8.07536 5.02424 8.83055 5.58104 9.38736C6.13785 9.94417 6.89305 10.257 7.68049 10.257Z" stroke="white" stroke-width="1.1134" stroke-linecap="round" stroke-linejoin="round"/><path d="M1 10.2569V4.31875C1 3.33445 1.39101 2.39045 2.08702 1.69445C2.78303 0.998436 3.72702 0.607422 4.71133 0.607422H10.6495C11.6338 0.607422 12.5778 0.998436 13.2738 1.69445C13.9698 2.39045 14.3608 3.33445 14.3608 4.31875V10.2569C14.3608 11.2412 13.9698 12.1852 13.2738 12.8812C12.5778 13.5772 11.6338 13.9682 10.6495 13.9682H4.71133C3.72702 13.9682 2.78303 13.5772 2.08702 12.8812C1.39101 12.1852 1 11.2412 1 10.2569Z" stroke="white" stroke-width="1.1134"/><path d="M11.7629 3.21457L11.772 3.20459" stroke="white" stroke-width="1.1134" stroke-linecap="round" stroke-linejoin="round"/><path d="M66.1844 0C66.6139 0 67.0259 0.170624 67.3296 0.474337C67.6333 0.778051 67.8039 1.18997 67.8039 1.61949V12.9559C67.8039 13.3854 67.6333 13.7974 67.3296 14.1011C67.0259 14.4048 66.6139 14.5754 66.1844 14.5754H54.848C54.4185 14.5754 54.0066 14.4048 53.7029 14.1011C53.3991 13.7974 53.2285 13.3854 53.2285 12.9559V1.61949C53.2285 1.18997 53.3991 0.778051 53.7029 0.474337C54.0066 0.170624 54.4185 0 54.848 0H66.1844ZM65.7796 12.551V8.2594C65.7796 7.55929 65.5014 6.88785 65.0064 6.3928C64.5113 5.89775 63.8399 5.61963 63.1398 5.61963C62.4515 5.61963 61.6499 6.0407 61.2612 6.6723V5.77348H59.002V12.551H61.2612V8.559C61.2612 7.9355 61.7632 7.42536 62.3867 7.42536C62.6874 7.42536 62.9757 7.5448 63.1883 7.7574C63.4009 7.97 63.5204 8.25834 63.5204 8.559V12.551H65.7796ZM56.3703 4.50218C56.7311 4.50218 57.0771 4.35886 57.3323 4.10374C57.5874 3.84862 57.7307 3.5026 57.7307 3.14181C57.7307 2.38875 57.1234 1.77334 56.3703 1.77334C56.0074 1.77334 55.6593 1.91752 55.4027 2.17416C55.146 2.43079 55.0019 2.77887 55.0019 3.14181C55.0019 3.89487 55.6173 4.50218 56.3703 4.50218ZM57.4959 12.551V5.77348H55.2529V12.551H57.4959Z" fill="white"/></svg>
+        </div>
+        <div className="font-poppins absolute inset-x-96 bottom-96 flex h-[770px] items-center justify-center font-bold tracking-[-0.96px] text-white" >
+          <div className="flex flex-grow flex-col items-center justify-between gap-y-16 self-stretch" >
+            <div className="flex justify-center self-stretch text-center text-5xl leading-[normal]" >
+              Premium Package
+            </div>
+            <div className="flex w-96 flex-grow items-start justify-center rounded-3xl bg-gradient-to-b from-black to-white/0 pb-28" >
+              <div className="flex flex-grow flex-col items-center justify-center gap-y-2.5 self-stretch" >
+                <div className="flex justify-center text-center text-5xl leading-[normal]" >
+                  10,799 L . E
+                </div>
+                <div className="h-0.5 w-96">
+                  <svg width="100%" height="100%" style={{"overflow":"visible"}} preserveAspectRatio="none" viewBox="0 2 393.99 1.5" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0 4.5L393.992 3" stroke="url(#paint0_linear_249_309)" stroke-width="5" stroke-linecap="round"/><defs><linearGradient id="paint0_linear_249_309" x1="197.002" y1="5.25001" x2="197.006" y2="6.25" gradientUnits="userSpaceOnUse"><stop stop-color="#A1A1A1"/><stop offset="1" stop-opacity="0"/></linearGradient></defs></svg>
+                </div>
+                <div className="flex flex-grow self-stretch pt-5 text-left text-[32px] leading-[normal]" >
+                  <ul className="ml-[1em] list-disc">
+                    <li className="mb-0">
+                      Advanced e-commerce functionality (unlimited products)
+                    </li>
+                    <li className="mb-0">Performance optimization</li>
+                    <li className="mb-0">
+                      Social media integration strategy
+                    </li>
+                    <li className="mb-0">Priority support hotline</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   );
-};
+}
 
-export default Navbar;
+interface PricingPageProps {
+  className: string;
+}
+
+/**
+ * This component was generated from Figma with FireJet.
+ * Learn more at https://www.firejet.io
+ *
+ * README:
+ * The output code may look slightly different when copied to your codebase. To fix this:
+ * 1. Include the necessary fonts. The required fonts are imported from public/index.html
+ * 2. Include the global styles. They can be found in App.css
+ *
+ * Note: Step 2 is not required for tailwind.css output
+ */
+      </div>
+
+      {/* Added Link component for the Navbar Pricing button */}
+      <Link href="/pricing">
+        <a className="fixed bottom-10 right-10 z-[1] bg-transparent padding-10 cursor-pointer bg-black hover:bg-[#2E2E2E] rounded-xl text-white py-2 px-5">
+          Go to Pricing
+        </a>
+      </Link>
+    </div>
+  );
+}
+
+interface PricingPageProps {
+  className: string;
+}
+
+export default PricingPage; // Move the export statement to the end of the file
