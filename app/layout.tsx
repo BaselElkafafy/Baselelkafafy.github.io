af254c156b3744667ac9e642ae39f736be758166
Basel Elkafafy <154018816+BaselElkafafy@users.noreply.github.com>
2023-12-24 11:41:33 -0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
-import { Analytics } from '@vercel/analytics/react';
-
+import { Analytics } from '@vercel/analytics';
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
