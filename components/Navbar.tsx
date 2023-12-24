6d389473b52521aabd46206eccfc526847089dc2
Basel Elkafafy <154018816+BaselElkafafy@users.noreply.github.com>
2023-12-24 10:12:44 -0800

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,21 @@
-"use client";
-
 import Image from "next/image";
 import React from "react";
+import { useRouter } from "next/router";
 
 const Navbar = () => {
+  const router = useRouter();
+
+  const navigateToPricing = () => {
+    router.push("/pricing");
+  };
+
+  const buttonStyle =
+    "z-[1] bg-transparent padding-10 cursor-pointer bg-black hover:bg-[#2E2E2E] rounded-xl text-white py-2 px-5";
+
   return (
     <div className="w-full h-[65px] bg-['#111'] fixed backdrop-blur-sm z-50 px-10">
       <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
-        <a
-          href="#about-me"
-          className="h-auto w-auto flex flex-row items-center"
-        >
+        <a href="#about-me" className="h-auto w-auto flex flex-row items-center">
           <Image
             src="/Logo.svg"
             alt="logo"
@@ -23,9 +28,14 @@ const Navbar = () => {
         <div className="flex flex-row gap-5">
           <div
             onClick={() => window.open("mailto:official.baselhassan@gmail.com")}
-            className=" z-[1] bg-transparent  padding-10 cursor-pointer bg-black hover:bg-[#2E2E2E] rounded-xl  text-white  py-2 px-5"
+            className={buttonStyle}
           >
             Contact
+          </div>
+
+          {/* New button to navigate to Pricing page with the same style */}
+          <div onClick={navigateToPricing} className={buttonStyle}>
+            Pricing
           </div>
         </div>
       </div>
