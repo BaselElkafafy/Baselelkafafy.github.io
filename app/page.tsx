02611be8a73e7a77ff7ff885c75da443cfb1e128
Basel Elkafafy <154018816+BaselElkafafy@users.noreply.github.com>
2023-12-27 02:28:51 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import Projects from "@/components/Projects";
 
 export default function Home() {
   return (
-    <main className="h-full w-full  bg-[url('/LooperGroup2.png')] bg-no-repeat">
+    <main className="h-full w-full  bg-[url('/LooperGroup2.webp')] bg-no-repeat">
       <div className="flex flex-col gap-20">
         <Banner />
         <About />
