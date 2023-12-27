57afa1e21ec8bdcd934c9d9f74ff2f9aeb47800d
Basel Elkafafy <154018816+BaselElkafafy@users.noreply.github.com>
2023-12-27 02:48:40 -0800

    Update Banner.tsx

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -8,7 +8,7 @@ const Banner = () => {
     <div className="flex flex-row items-center justify-center px-20 mt-[150px] z-[20] ">
       <div className="flex flex-col  justify-center  text-center">
         <div className="justify-center flex ">
-          <Image src="/Me.png" height={300} width={300} alt="Basel Elkafafy" />
+          <Image src="/Me.webp" height={300} width={300} alt="Basel Elkafafy" />
         </div>
 
         <div className="flex flex-col gap-6 mt-6 cursor-pointer hover:-translate-y-1  transition  hover:scale-110 transition ease-in-out hover:text-transparent bg-clip-text  hover:bg-gradient-to-r from-purple-500 to-orange-400 z-[1] tracking-tighter text-7xl font-semibold text-white max-w-[600px] w-auto h-auto">
