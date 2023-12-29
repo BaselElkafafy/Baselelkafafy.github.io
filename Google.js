d07e79e54693a61dce1d2c534f74c553f4330ac1
Basel Elkafafy <154018816+BaselElkafafy@users.noreply.github.com>
2023-12-29 07:50:56 -0800

    Create Google.js

---
+++ Google.js
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-FSX55Q0Z1N"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-FSX55Q0Z1N');
+</script>
