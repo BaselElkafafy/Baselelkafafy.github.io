0fcc95b86a843a2f9ba23f68e9c15b018f10bb97
Basel Elkafafy <154018816+BaselElkafafy@users.noreply.github.com>
2024-01-01 03:24:08 -0800

    Update Credentials.tsx

--- components/Credentials.tsx
+++ components/Credentials.tsx
@@ -6,7 +6,7 @@ const Projects = () => {
     <>
       <section id="projects">
         <h1 className="text-white font-semibold text-center text-6xl pt-[35px]">
-          CERTIFICATES
+          CREDINTIALS
         </h1>
         <p className="tracking-[0.5em] text-center text-transparent font-light pb-5 bg-clip-text bg-gradient-to-r from-purple-700 to-orange-500 text-xl">
           EXPLORE MY CERTIFICATES
