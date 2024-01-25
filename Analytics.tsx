bf945c84c12c5f807ce187d9f16c281f977c6d20
Basel Elkafafy <154018816+BaselElkafafy@users.noreply.github.com>
2024-01-26 00:48:42 +0200

    Create Analytics.tsx

---
+++ Analytics.tsx
@@ -0,0 +1,10 @@
+// src/components/Analytics.tsx
+
+import { Analytics } from '@vercel/analytics/react';
+import React from 'react';
+
+const AnalyticsComponent: React.FC = () => {
+  return <Analytics />;
+};
+
+export default AnalyticsComponent;
