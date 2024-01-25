7596bdfa083130e0e565803939b2d2e013fb0e95
Basel Elkafafy <154018816+BaselElkafafy@users.noreply.github.com>
2024-01-25 10:56:45 -0800

    Update ECommerce.tsx

--- components/ECommerce.tsx
+++ components/ECommerce.tsx
@@ -8,11 +8,11 @@ const ECommerce = () => {
       <h1 className="text-white font-semibold text-center text-6xl pt-[35px]">
         Ecommerce
       </h1>
-      <p className=" tracking-[0.5em] text-center text-transparent font-light pb-5  bg-clip-text bg-gradient-to-r from-purple-700 to-orange-500  text-1xl ">
+      <p className="tracking-[0.5em] text-center text-transparent font-light pb-5 bg-clip-text bg-gradient-to-r from-purple-700 to-orange-500 text-1xl">
         EXPLORE NOW
       </p>
-      <div className=" container mx-auto 2xl  ">
-        <div className="flex-col flex md:flex-row  mt-7">
+      <div className="container mx-auto 2xl">
+        <div className="flex-col flex md:flex-row mt-7">
           <Link
             href="https://breatheeveryday.com/"
             rel="noopener noreferrer"
