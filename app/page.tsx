a0dc2edf9b59df3147373d2a8a2a37ecd56671c9
Basel Elkafafy <154018816+BaselElkafafy@users.noreply.github.com>
2024-05-25 18:15:39 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,10 @@
-import About from "@/components/About";
 import Banner from "@/components/Banner";
-import Experience from "@/components/Experience";
 import Footer from "@/components/Footer";
-import Credentials from "@/components/Credentials";
-import EgyptianProjects from "@/components/EgyptianProjects";
-import ECommerce from "@/components/ECommerce";
-import Projects from "@/components/Projects";
-
 export default function Home() {
   return (
     <main className="h-full w-full  bg-[url('/LooperGroup2.webp')] bg-no-repeat">
       <div className="flex flex-col gap-20">
         <Banner />
-        <About />
-        <Experience />
-        <ECommerce />
-        <EgyptianProjects />
-        <Projects />
-        <Credentials />
         <Footer />
       </div>
     </main>
