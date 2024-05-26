e3ec99fafdb70df71e5c534899d4efe2fe900dc7
Basel Elkafafy <154018816+BaselElkafafy@users.noreply.github.com>
2024-05-27 00:52:55 +0300

    Update Banner.tsx

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -25,7 +25,10 @@ const Banner: React.FC = () => {
           🍿
         </p>
         <div className="text-md flex justify-center">
-          <button onClick={openWebsite} className="bg-white text-black rounded-lg px-4 py-2">
+          <button
+            onClick={openWebsite}
+            className="bg-white text-black rounded-[20px] px-4 py-2"
+          >
             Visit my website
           </button>
         </div>
